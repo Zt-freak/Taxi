de4d4a5f457fe3f941d681d7821094dcdafe8c20
Zt-freak <bnkho98@hotmail.com>
2021-01-08 16:19:58 +0100

    iniital commit

---
+++ Taxi/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿using System.Windows.Forms;
+
+namespace Taxi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Info = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(13, 13);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "yyyy-MM-dd HH:mm";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(240, 13);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 1;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Info,
+            this.Quantity});
+            this.dataGridView1.Location = new System.Drawing.Point(14, 82);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(427, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Info
+            // 
+            this.Info.HeaderText = "Info";
+            this.Info.Name = "Info";
+            // 
+            // Quantity
+            // 
+            this.Quantity.HeaderText = "Quantity";
+            this.Quantity.Name = "Quantity";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Location = new System.Drawing.Point(240, 46);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Kilometres travelled";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 244);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Info;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+    }
+}
+
---
+++ Taxi/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taxi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            this.dataGridView1.Rows.Clear();
+            DateTime startDateTime = this.dateTimePicker1.Value;
+            DateTime endDateTime = this.dateTimePicker2.Value;
+            TimeSpan travelTime = endDateTime - startDateTime;
+            DateTime discountStartDateStartTime = startDateTime.Date.AddHours(8);
+            DateTime discountStartDateEndTime = startDateTime.Date.AddHours(18);
+            DateTime discountEndDateStartTime = endDateTime.Date.AddHours(8);
+            DateTime discountEndDateEndTime = endDateTime.Date.AddHours(18);
+            TimeSpan dateDifference = endDateTime.Date - startDateTime.Date;
+            decimal KmTravelled = this.numericUpDown1.Value;
+
+            Console.WriteLine(discountStartDateStartTime);
+            Console.WriteLine(discountStartDateEndTime);
+            Console.WriteLine(discountEndDateStartTime);
+            Console.WriteLine(discountEndDateEndTime);
+
+            // display distance travelled
+            DataGridViewRow KmTravelledRow = (DataGridViewRow)this.dataGridView1.Rows[0].Clone();
+            KmTravelledRow.Cells[0].Value = "Distance travelled (km)";
+            KmTravelledRow.Cells[1].Value = KmTravelled;
+            this.dataGridView1.Rows.Add(KmTravelledRow);
+
+            // display average speed
+            if (travelTime.TotalHours > 0)
+            {
+                DataGridViewRow averageSpeedRow = (DataGridViewRow)this.dataGridView1.Rows[0].Clone();
+                averageSpeedRow.Cells[0].Value = "Avg. speed (km/h)";
+                averageSpeedRow.Cells[1].Value = KmTravelled/ Convert.ToDecimal(travelTime.TotalHours);
+                this.dataGridView1.Rows.Add(averageSpeedRow);
+            }
+
+            // if startDateTime and endDateTime are the on the same day.
+            if (dateDifference.TotalDays == 0)
+            {
+                DateTime discountStartTime;
+                if (startDateTime < discountStartDateStartTime) {
+                    discountStartTime = discountStartDateStartTime;
+                }
+                else
+                {
+                    discountStartTime = startDateTime;
+                }
+
+                DateTime discountEndTime;
+                if (endDateTime < discountStartDateEndTime) {
+                    discountEndTime = endDateTime;
+                }
+                else
+                {
+                    discountEndTime = discountStartDateEndTime;
+                }
+                TimeSpan intersection = discountEndTime - discountStartTime;
+                double discountMinutes = Math.Round(intersection.TotalMinutes);
+                double normalMinutes = Math.Round((travelTime - intersection).TotalMinutes);
+
+                DataGridViewRow DiscountTimeRow = (DataGridViewRow)this.dataGridView1.Rows[0].Clone();
+                DiscountTimeRow.Cells[0].Value = "Discount time (minutes)";
+                DiscountTimeRow.Cells[1].Value = discountMinutes;
+                this.dataGridView1.Rows.Add(DiscountTimeRow);
+
+                DataGridViewRow NormalTimeRow = (DataGridViewRow)this.dataGridView1.Rows[0].Clone();
+                NormalTimeRow.Cells[0].Value = "Normal time (minutes)";
+                NormalTimeRow.Cells[1].Value = normalMinutes;
+                this.dataGridView1.Rows.Add(NormalTimeRow);
+
+                DataGridViewRow DiscountPriceRow = (DataGridViewRow)this.dataGridView1.Rows[0].Clone();
+                DiscountPriceRow.Cells[0].Value = "Discount time price (EUR)";
+                DiscountPriceRow.Cells[1].Value = discountMinutes * 0.25;
+                this.dataGridView1.Rows.Add(DiscountPriceRow);
+
+                DataGridViewRow NormalPriceRow = (DataGridViewRow)this.dataGridView1.Rows[0].Clone();
+                NormalPriceRow.Cells[0].Value = "Normal time price (EUR)";
+                NormalPriceRow.Cells[1].Value = normalMinutes * 0.45;
+                this.dataGridView1.Rows.Add(NormalPriceRow);
+
+                DataGridViewRow TotalPriceRow = (DataGridViewRow)this.dataGridView1.Rows[0].Clone();
+                TotalPriceRow.Cells[0].Value = "Total time price (EUR)";
+                TotalPriceRow.Cells[1].Value = (double) KmTravelled + discountMinutes * 0.25 + normalMinutes * 0.45;
+                this.dataGridView1.Rows.Add(TotalPriceRow);
+            }
+            
+            /*if (endDateTime.Hour > 8 && endDateTime.Hour < 18)
+            {
+                Console.WriteLine(endDateTime.TimeOfDay);
+            }*/
+        }
+    }
+}
